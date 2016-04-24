3ec63a77b73b7efb6ea2068b5594fa733ab23079
Shuai <sudapink@gmail.com>
2016-04-23 20:40:21 -0700

    first update

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,5 @@
-
+dir<-"/UCI HAR Dataset"
+files<-list.files(path="./UCI HAR Dataset/train/Inertial Signals")
+subfolder<-"./UCI HAR Dataset/train/"
+full_name<-paste0(subfolder,"X_train.txt")
+testData <-read.table(full_name,header=F)
